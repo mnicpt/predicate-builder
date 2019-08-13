0ee5c4c7150c843b55cfdaf55f2e3c0723af5402
Steve Mask <mnicpt@gmail.com>
2019-08-13 13:52:58 -0400

    QueryBuilder and Query

---
+++ src/QueryBuilder/conditions.js
@@ -0,0 +1,17 @@
+const conditions = {
+  numbers: [
+    'equals',
+    'between',
+    'greater_than',
+    'less_than',
+    'in_list'
+  ],
+  string: [
+    'equals',
+    'contains',
+    'starts_with',
+    'in_list'
+  ]
+};
+
+export default conditions;
