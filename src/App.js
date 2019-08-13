fd05300c9648c1b9a19831b8e0fe02501e97edc6
Steve Mask <mnicpt@gmail.com>
2019-08-13 13:52:20 -0400

    data init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import names from './names';
+import conditions from './conditions';
+import QueryBuilder from './QueryBuilder';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Search for Sessions</h1>
       </header>
+      <main>
+        <QueryBuilder names={names} conditions={conditions} />
+      </main>
     </div>
   );
 }
